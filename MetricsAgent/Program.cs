c6a3bf96e3d8ba0a2cea32dc8758c59eae76c8f9
Imran Irfan <imranbirfan@gmail.com>
2025-04-23 16:54:43 -0700

    Update for testing

--- MetricsAgent/Program.cs
+++ MetricsAgent/Program.cs
@@ -15,6 +15,18 @@ class MetricsAgent
         Console.WriteLine($"Total Physical Memory: {metric.TotalPhysicalMemory / (1024 * 1024)} MB");
         Console.WriteLine($"Available Physical Memory: {metric.AvailablePhysicalMemory / (1024 * 1024)} MB");
         Console.WriteLine($"CPU Load: {metric.CPULoad} %");
+        Console.WriteLine("Drives:");
+        foreach (var drive in metric.Drives)
+        {
+            Console.WriteLine($"  Drive Name: {drive.Name}");
+            Console.WriteLine($"  Drive Type: {drive.Type}");
+            Console.WriteLine($"  Volume: {drive.Volume}");
+            Console.WriteLine($"  File System: {drive.FileSystem}");
+            Console.WriteLine($"  Available Free Space: {drive.AvailableFreeSpace / (1024 * 1024)} MB");
+            Console.WriteLine($"  Total Space: {drive.TotalSpace / (1024 * 1024)} MB");
+            Console.WriteLine($"  Used Space: {drive.UsedSpace / (1024 * 1024)} MB");
+            Console.WriteLine();
+        }
 
         // We basically want to take the information from Metric, JSONIFY it and send it to an external API service.
     }
